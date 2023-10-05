097f966c965bc5ef68dd1a9a100f959219258e50
kishibee <raflyade9@gmail.com>
2023-10-05 10:01:28 -0700

    layouting

---
+++ app/src/main/java/com/example/storyapp/presentation/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.storyapp.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.storyapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/storyapp/presentation/welcome/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.storyapp.presentation.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.storyapp.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
